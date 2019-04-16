5a11ecf86fbeb3284c5ff88a59923c2a0fff9403
Elias Paiva <elias.paiva@IDEAGRI.LAN>
2019-04-16 15:15:01 -0300

    Validação de vitória

--- Jogo/jogodavelha.js
+++ Jogo/jogodavelha.js
@@ -1,7 +1,59 @@
-var jogadorAtivo = 'X';
+var jogadorAtivo = '';
+var matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
 
-function trocaJogador() {
-    if(jogadorAtivo !== 'X'){
+function verificaVitoria(){
+    let soma0 = matriz[0][0] + matriz[0][1] + matriz[0][2];
+    let soma1 = matriz[1][0] + matriz[1][1] + matriz[1][2];
+    let soma2 = matriz[2][0] + matriz[2][1] + matriz[2][2];
+    let soma3 = matriz[0][0] + matriz[1][1] + matriz[2][2];
+    let soma4 = matriz[0][2] + matriz[1][1] + matriz[2][0];
+
+    if(soma0 == 3 || soma0 == -3){
+        vitoria(0);
+    }else if(soma1 == 3 || soma1 == -3){
+        vitoria(1);
+    }else if(soma2 == 3 || soma2 == -3){
+        vitoria(2);
+    }else if(soma3 == 3 || soma3 == -3){
+        vitoria(3);
+    }else if(soma4 == 3 || soma4 == -3){
+        vitoria(4);
+    }
+}
+
+function vitoria(tipo) {
+    let casas;
+    switch (tipo){
+        case 0 :{
+            casas = [0, 1, 2];
+            break;
+        }
+        case 1 :{
+            casas = [3, 4, 5];
+            break;
+        }
+        case 2 :{
+            casas = [6, 7, 8];
+            break;
+        }
+        case 3 :{
+            casas = [0, 4, 8];
+            break;
+        }
+        case 4 :{
+            casas = [2, 4, 6];
+            break;
+        }
+    }
+    desabilitarTodos();
+
+    document.getElementById(casas[0]).style.backgroundColor = 'red';
+    document.getElementById(casas[1]).style.backgroundColor = 'red';
+    document.getElementById(casas[2]).style.backgroundColor = 'red';
+}
+
+function trocaJogador(forcar) {
+    if(jogadorAtivo !== 'X' || forcar){
         jogadorAtivo = 'X';
     }else{
         jogadorAtivo = 'O';
@@ -12,14 +64,27 @@ function trocaJogador() {
 
 function clickou(botao) {
     botao.value = jogadorAtivo;
-    console.log(Math.trunc(botao.id / 3));
-    console.log(botao.id % 3);
-    trocaJogador();
+
+    matriz[Math.trunc(botao.id / 3)][botao.id % 3] = jogadorAtivo === 'X' ? 1 : -1;
+    botao.disabled = true;
+
+    verificaVitoria();
+    trocaJogador(false);
 }
 
 function limpar() {
     document.querySelectorAll("input[class$='casa']").forEach(element => {
         element.value = '';
+        element.disabled = false;
+        element.style.backgroundColor = '';
+    });
+    matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]];
+    trocaJogador(true);
+}
+
+function desabilitarTodos(){
+    document.querySelectorAll("input[class$='casa']").forEach(element => {
+        element.disabled = true;
     });
 }
 
