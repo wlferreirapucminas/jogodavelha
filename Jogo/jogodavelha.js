bbd7d5d8a53a296083db3c7ed0ea568232e3db74
Elias Paiva <elias.paiva@IDEAGRI.LAN>
2019-04-17 15:05:50 -0300

    Conclusão

--- Jogo/jogodavelha.js
+++ Jogo/jogodavelha.js
@@ -1,50 +1,71 @@
 var jogadorAtivo = '';
 var matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
+var placar = [0, 0]
+
+function atualizaPlacar() {
+    if(jogadorAtivo == 'X'){
+        placar[0]++;
+        document.getElementById('placarX').textContent = "X : " + placar[0];
+    }else{
+        placar[1]++;
+        document.getElementById('placarO').textContent = "O : " + placar[1];
+    }
+}
 
 function verificaVitoria(){
+    //linhas
     let soma0 = matriz[0][0] + matriz[0][1] + matriz[0][2];
     let soma1 = matriz[1][0] + matriz[1][1] + matriz[1][2];
     let soma2 = matriz[2][0] + matriz[2][1] + matriz[2][2];
-    let soma3 = matriz[0][0] + matriz[1][1] + matriz[2][2];
-    let soma4 = matriz[0][2] + matriz[1][1] + matriz[2][0];
+
+    //colunas
+    let soma3 = matriz[0][0] + matriz[1][0] + matriz[2][0];
+    let soma4 = matriz[0][1] + matriz[1][1] + matriz[2][1];
+    let soma5 = matriz[0][2] + matriz[1][2] + matriz[2][2];
+
+    //diagonais
+    let soma6 = matriz[0][0] + matriz[1][1] + matriz[2][2];
+    let soma7 = matriz[0][2] + matriz[1][1] + matriz[2][0];
 
     if(soma0 == 3 || soma0 == -3){
         vitoria(0);
+        return true;
     }else if(soma1 == 3 || soma1 == -3){
         vitoria(1);
+        return true;
     }else if(soma2 == 3 || soma2 == -3){
         vitoria(2);
+        return true;
     }else if(soma3 == 3 || soma3 == -3){
         vitoria(3);
+        return true;
     }else if(soma4 == 3 || soma4 == -3){
         vitoria(4);
+        return true;
+    }else if(soma5 == 3 || soma5 == -3){
+        vitoria(5);
+        return true;
+    }else if(soma6 == 3 || soma6 == -3){
+        vitoria(6);
+        return true;
+    }else if(soma7 == 3 || soma7 == -3){
+        vitoria(7);
+        return true;
     }
+    return false;
 }
 
 function vitoria(tipo) {
     let casas;
-    switch (tipo){
-        case 0 :{
-            casas = [0, 1, 2];
-            break;
-        }
-        case 1 :{
-            casas = [3, 4, 5];
-            break;
-        }
-        case 2 :{
-            casas = [6, 7, 8];
-            break;
-        }
-        case 3 :{
-            casas = [0, 4, 8];
-            break;
-        }
-        case 4 :{
-            casas = [2, 4, 6];
-            break;
-        }
-    }
+    if     (tipo == 0){ casas = [0, 1, 2]; }
+    else if(tipo == 1){ casas = [3, 4, 5]; }
+    else if(tipo == 2){ casas = [6, 7, 8]; }
+    else if(tipo == 3){ casas = [0, 3, 6]; }
+    else if(tipo == 4){ casas = [1, 4, 7]; }
+    else if(tipo == 5){ casas = [2, 5, 8]; }
+    else if(tipo == 6){ casas = [0, 4, 8]; }
+    else if(tipo == 7){ casas = [2, 4, 6]; }
+
     desabilitarTodos();
 
     document.getElementById(casas[0]).style.backgroundColor = 'red';
@@ -52,8 +73,8 @@ function vitoria(tipo) {
     document.getElementById(casas[2]).style.backgroundColor = 'red';
 }
 
-function trocaJogador(forcar) {
-    if(jogadorAtivo !== 'X' || forcar){
+function trocaJogador() {
+    if(jogadorAtivo !== 'X'){
         jogadorAtivo = 'X';
     }else{
         jogadorAtivo = 'O';
@@ -68,8 +89,10 @@ function clickou(botao) {
     matriz[Math.trunc(botao.id / 3)][botao.id % 3] = jogadorAtivo === 'X' ? 1 : -1;
     botao.disabled = true;
 
-    verificaVitoria();
-    trocaJogador(false);
+    if(verificaVitoria()){
+        atualizaPlacar();
+    }
+    trocaJogador();
 }
 
 function limpar() {
@@ -79,7 +102,6 @@ function limpar() {
         element.style.backgroundColor = '';
     });
     matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]];
-    trocaJogador(true);
 }
 
 function desabilitarTodos(){
@@ -88,4 +110,10 @@ function desabilitarTodos(){
     });
 }
 
-window.onload = trocaJogador;
+function preparaTela() {
+    document.getElementById('placarX').textContent = "X : " + placar[0]
+    document.getElementById('placarO').textContent = "O : " + placar[1]
+    trocaJogador();
+}
+
+window.onload = preparaTela;
